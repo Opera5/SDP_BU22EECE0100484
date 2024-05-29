a18dc6e811c7a500261f4e7872c786bf542403db
Opera5 <ajamiu46@gmail.com>
2024-05-29 11:14:01 +0530

    29/5/24 11:13

--- IT track/Emb_keil/blink_LED.c
+++ IT track/Emb_keil/blink_LED.c
@@ -2,6 +2,7 @@
 sbit sw1 = P2^1;
 sbit sw2 = P2^2;
 
+unsigned int b;
 void delay(unsigned int);
 void main (void)
 	{
@@ -12,6 +13,8 @@ void main (void)
 		
 		while(1)
 			{
+				//P1 = 0x00;
+				/*
 					if (sw1 == 0 && sw2 == 0 )
 						{
 								P1 = 0x00;
@@ -40,9 +43,22 @@ void main (void)
 								P1 = 0x00;
 								delay(500);
 					
-					}
-		}
+					}*/
+				
 
+		 
+  for(b = 0; b < 8; b++)
+  {    
+  const unsigned int mask = 1 << b;
+       unsigned short num = 0x00;
+       num |= mask;
+       
+      // PORTB = mask;
+		P1 = num;
+       delay(500);
+    }
+		
+		}
 }
 	
 void delay(unsigned int t)
