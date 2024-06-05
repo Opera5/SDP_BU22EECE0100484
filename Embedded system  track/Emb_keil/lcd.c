15779f38874a609775969d2787ef137edbfcdb6c
Opera5 <ajamiu46@gmail.com>
2024-06-06 04:26:25 +0530

    report

--- Embedded system  track/Emb_keil/lcd.c
+++ Embedded system  track/Emb_keil/lcd.c
@@ -1,48 +1,34 @@
-#include <reg51.h>
-sbit rs=P2^0;
-sbit rw=P2^1;
-sbit e=P2^2;
-void delay(unsigned int);
-void WriteCommandToLCD(unsigned char ch);
-void WriteDataToLCD(unsigned char ch);
-void WriteStringToLCD(unsigned char ch[]);
-void main(void)
-{
-	//	unsigned char ch[]="ES TRAINING";
-	unsigned char ch1[]="GITAM UNIVERSITY, BANGALORE";
-	unsigned int j,k;
-	unsigned int MyData = 20;
-		 
-//LCD Intialization
-	WriteCommandToLCD(0x38); //2 lines and 5×7 matrix
-	WriteCommandToLCD(0x01); //Clear display screen
-	WriteCommandToLCD(0x0E); //Display on, cursor off
-	WriteCommandToLCD(0x80); //Force cursor to the beginning ( 1st line)
-	WriteCommandToLCD(0x06); //Increment cursor (shift cursor to right)
+# include<reg51.h>
 
-//Sending Data to LCD
-	WriteStringToLCD("ES TRAINING"); //Sending the string to LCD
-	WriteCommandToLCD(0xc0); //Force cursor to the beginning ( 2nd line)
-	for(j=0;ch1[j]!='\0';j++)
-		{
-			WriteDataToLCD(ch1[j]); // Sending one character to LCD
-		}
-	for(k=0;k<30;k++)
-		{
-			WriteCommandToLCD(0x1c);// shift entire dispaly right
-		}
-	while(1)
-		{
-			WriteCommandToLCD(0x01); //Clear display screen
-			WriteCommandToLCD(0x80); //Force cursor to the beginning ( 1st line)
-			WriteStringToLCD("CLASS STRENGTH");//Sending String to LCD
-			WriteCommandToLCD(0xc0); //Force cursor to the beginning ( 2nd line)
-			WriteDataToLCD((MyData / 10) + 48);//seperating the first digit of MyData
-			WriteDataToLCD((MyData % 10) + 48);//seperating the first digit of MyData
-			WriteStringToLCD(" STUDENTS"); //Sending String to LCD
-		}
-	//}
-}
+#include<stdio.h>
+
+
+#define lcdport P1  //lcd connection
+
+//LED connections assignment
+sbit L1 = P0^3;
+sbit L2 = P2^6;
+sbit L3 = P2^7;
+
+// LCD connection
+sbit rs=P2^0;
+sbit en=P2^1;
+
+//ADC connections
+ sbit SC = P2^2;
+ sbit ALE = P2^3;
+ sbit OE = P2^4;
+ sbit EOC = P2^5; 
+ 
+//Declaring the input selection pin
+ sbit ADDR_A = P1^0;
+ sbit ADDR_B = P1^1;
+ sbit ADDR_C = P1^2;
+
+unsigned int result[3];
+unsigned char number;
+unsigned int MyData = 20;
+
 void delay(unsigned int t)////Function for setting 1ms delay
 {
 	unsigned int i,j;
@@ -50,29 +36,165 @@ void delay(unsigned int t)////Function for setting 1ms delay
 	for(j=0;j<1275;j++);
 }
 
-void WriteCommandToLCD(unsigned char ch) //Function for sending Command
+
+void daten()
+
 {
-	e=1; 
-	rs=0;
-	rw=0;
-	P1=ch;
-	e=0;
-	delay(20);
+
+    rs=1;
+
+    //rw=0;
+
+    en=1;
+
+    delay(1);
+
+    en=0;
+
 }
-void WriteDataToLCD(unsigned char ch) //Function for sending Data
+
+
+void lcd_data(unsigned char ch)
+
 {
-	e=1;
-	rs=1;
-	rw=0;
-	P1=ch;
-	e=0;
-	delay(20);
+
+    lcdport=ch & 0xF0;
+
+    daten();
+
+    lcdport=ch<<4 & 0xF0;
+
+    daten();
+
 }
-void WriteStringToLCD(unsigned char ch[]) //Function for sending string
+
+
+
+void cmden(void)
+
 {
-	int i;
-	 for(i=0;ch[i]!='\0';i++)
-	{
-	    WriteDataToLCD(ch[i]);
-	}  
+
+    rs=0;
+
+    en=1;
+
+    delay(1);
+
+    en=0;
+
+}
+
+
+void lcdcmd(unsigned char ch)
+
+{
+
+
+    lcdport=ch & 0xf0;
+
+    cmden();
+
+    lcdport=ch<<4 & 0xF0;
+
+    cmden();
+
+}
+
+
+
+lcdprint(unsigned char *str)  //Function to send string data to LCD.
+
+{
+
+    while(*str)
+
+    {
+
+        lcd_data(*str);
+
+        str++;
+
+    }
+
+}
+
+
+void lcd_ini()  //Function to inisialize the LCD
+
+{
+
+    lcdcmd(0x02);
+
+    lcdcmd(0x28);
+
+    lcdcmd(0x0e);
+
+    lcdcmd(0x01);
+
+}
+
+
+void show()
+
+{ 
+
+   sprintf(result,"%d",number);
+
+   lcdprint(result);
+
+   lcdprint("  ");
+
+}
+
+
+
+
+void main()
+
+{
+//P2 = 0x00;
+ int i=0;
+	unsigned char ADC_Value = 0;
+	P2 = 0xFF; 
+	EOC = 1;
+	ALE = 0;
+	OE = 0;
+	SC = 0;
+	
+
+lcd_ini();
+
+lcdprint(" ADC 0808/0809  ");
+
+lcdcmd(192);
+
+lcdprint("  Interfacing   ");
+
+//delay(500);
+
+//lcdcmd(1);
+
+//lcdprint("Ch1   Ch2   Ch3 ");
+
+
+while(1)
+
+{
+
+	
+	delay(200);
+			lcdcmd(0x01); //Clear display screen
+			lcdcmd(0x80); //Force cursor to the beginning ( 1st line)
+			lcdprint("CLASS STRENGTH");//Sending String to LCD
+			lcdcmd(0xc0); //Force cursor to the beginning ( 2nd line)
+			
+			lcd_data((MyData/ 10) + 48);//seperating the first digit of MyData
+			lcd_data((MyData% 10) + 48);//seperating the first digit of MyData
+			
+			//sprintf(result,"%03d",number);
+			//lcdprint(result);
+			//lcdprint(" STUDENTS"); //Sending String to LCD
+
+}
+
 }
