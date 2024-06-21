61b743a629b86f99528c94dad189d27e18a541b7
Jamih <88864002+Opera5@users.noreply.github.com>
2024-06-21 15:48:50 +0530

    Update move_laser_calc.py

--- move_laser_calc.py
+++ move_laser_calc.py
@@ -25,3 +25,37 @@ moves(5)
 #print(fn)
 for i,n in zip(l,fn):
     print(f"Reading {i} is {n}")
+
+
+
+'''
+from time import sleep
+from robot_control_class import RobotControl
+
+rc = RobotControl(robot_name= "summit")
+
+def moves(secs):
+    rc.move_straight()
+    sleep(secs)
+    rc.stop_robot()
+
+def glaser(f,d,g):
+   
+    r1 = rc.get_laser_summit(f)
+    r2 = rc.get_laser_summit(d)
+    r3 = rc.get_laser_summit(g)
+    
+    return [r1,r2,r3]
+l = []
+for i in range(3):
+    en = int(input("enter any value from 0 - 719: "))
+    l.append(en)
+fn = glaser(l[0],l[1],l[2])
+secs = 0
+while secs < 5: 
+    moves(secs)
+#print(fn)
+    for i,n in zip(l,fn):
+        print(f"Reading {i} is {n}")
+    secs+=2
+    '''
